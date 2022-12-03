84273b1ca0f32ea4cddc2270497435e5fb39170a
rabbanigithub <rabbanir@hotmail.com>
2022-12-04 00:16:44 +0600

    update

--- modules/instances/instance.tf
+++ modules/instances/instance.tf
@@ -13,7 +13,7 @@ variable "VPC_ID" {
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "sshkey-${var.ENV}.pub"
 }
 
 data "aws_ami" "ubuntu" {
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -29,7 +29,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"]
 }
 
 resource "aws_instance" "instance" {
@@ -38,7 +38,7 @@ resource "aws_instance" "instance" {
 
   subnet_id = element(var.PUBLIC_SUBNETS, 0)
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
-  key_name = aws_key_pair.mykeypair.key_name
+  key_name = aws_key_pair.sshkeypair.key_name
 
   tags = {
     Name         = "instance-${var.ENV}"
