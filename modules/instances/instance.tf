69c52ba08128748cb23e656e69d0b7605b08e323
rabbanigithub <rabbanir@hotmail.com>
2022-12-04 00:10:01 +0600

    adding instance module

---
+++ modules/instances/instance.tf
@@ -0,0 +1,77 @@
+variable "ENV" {
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "PUBLIC_SUBNETS" {
+  type = list
+}
+
+variable "VPC_ID" {
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.INSTANCE_TYPE
+
+  subnet_id = element(var.PUBLIC_SUBNETS, 0)
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  key_name = aws_key_pair.mykeypair.key_name
+
+  tags = {
+    Name         = "instance-${var.ENV}"
+    Environmnent = var.ENV
+  }
+}
+
+resource "aws_security_group" "allow-ssh" {
+  vpc_id      = var.VPC_ID
+  name        = "allow-ssh-${var.ENV}"
+  description = "allow ssh and all egress traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name         = "allow-ssh"
+    Environmnent = var.ENV
+  }
+}
+
+resource "aws_key_pair" "sshkeypair" {
+  key_name   = "sshkeypair-${var.ENV}"
+  public_key = file("${path.root}/${var.PATH_TO_PUBLIC_KEY}")
+}
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,29 +1,40 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.0"
+variable "ENV" {
+}
 
-  name = local.name
+variable "AWS_REGION" {
+}
+
+module "main-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-${var.ENV}"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs             = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnets  = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
 
-  enable_ipv6 = false
-
   enable_nat_gateway = false
-  single_nat_gateway = true
+  enable_vpn_gateway = false
 
-  private_subnet_tags = {
-    Name = "terraform private"
-  }
-  public_subnet_tags = {
-    Name = "terraform public"
-  }
-
-  tags = local.tags
-
-  vpc_tags = {
-    Name = "terraform vpc"
+  tags = {
+    Terraform   = "true"
+    Environment = var.ENV
   }
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.main-vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.main-vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.main-vpc.public_subnets
+}
+
